bd7263e4d58e3e4911238d69cd460ee0edf399c8
Azzahabie <azzahabie@gmail.com>
2022-01-09 20:09:42 +0800

    hee

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://azzahabie.dev',
 ]
 
 django_heroku.settings(locals())
